b6a55005986e84904e27b99c794d783fe3e7ca8b
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2016-11-28 20:29:35 +0100

    WRIT-176 Load plugin multiple times

--- plugins/fd.js
+++ plugins/fd.js
@@ -1,4 +1,3 @@
-import Skeleton from './se.infomaker.skeleton/index'
 import TextAnalyzer from './se.infomaker.textanalyzer/index'
 import Preamble from './se.infomaker.preamble/Preamble'
 import PublishFlow from './se.infomaker.publishflow/index'
